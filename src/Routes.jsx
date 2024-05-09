76da993d11594e77e36a10cdb8f680cd215d7dca
Gibermar Regalado <gberxx@gmail.com>
2024-05-09 14:20:02 +0800

    Last Commit

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import ContactPage from "./components/ContactPage";
 import AboutPage from "./components/AboutPage";
+import NotFoundPage from "./components/NotFoundPage";
 
 const AppRoutes = () => {
   return (
@@ -11,9 +12,11 @@ const AppRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/contact" element={<ContactPage />} />
       <Route path="/about" element={<AboutPage />} />
+
+      {/* the last you can create element page of that */}
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
-
 
 export default AppRoutes;
